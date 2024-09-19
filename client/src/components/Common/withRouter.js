824c1f5a6d9491bf619c116421543ff96b17b52d
SmileAchiever <128380874+ApplyCode@users.noreply.github.com>
2024-09-19 17:33:01 -0400

    Update withRouter.js

--- client/src/components/Common/withRouter.js
+++ client/src/components/Common/withRouter.js
@@ -9,7 +9,12 @@ function withRouter(Component) {
         let location = useLocation();
         let navigate = useNavigate();
         let params = useParams();
-
+        return (
+            <Component
+                {...props}
+                router={{ location, navigate, params }}
+            />
+        );
     }
 
     return ComponentWithRouterProp;
