423a2224843c89254fbe5a7da990d675cc6dd93c
SmileAchiever <128380874+ApplyCode@users.noreply.github.com>
2024-09-19 10:09:11 -0400

    Update Breadcrumb.js

--- client/src/components/Common/Breadcrumb.js
+++ client/src/components/Common/Breadcrumb.js
@@ -2,14 +2,7 @@ import React, { useState } from "react"
 import PropTypes from "prop-types"
 import { Link } from "react-router-dom"
 import {
-  Row,
-  Col,
-  BreadcrumbItem,
-  Dropdown,
-  DropdownToggle,
-  DropdownItem,
-  DropdownMenu,
-  Button,
+
 } from "reactstrap"
 
 const Breadcrumb = props => {
