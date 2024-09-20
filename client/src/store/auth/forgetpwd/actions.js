8d80be091d73faff891289becee680a14cb48d95
SmileAchiever <128380874+ApplyCode@users.noreply.github.com>
2024-09-20 03:53:12 -0400

    Update actions.js

--- client/src/store/auth/forgetpwd/actions.js
+++ client/src/store/auth/forgetpwd/actions.js
@@ -11,12 +11,7 @@ export const userForgetPassword = (user, history) => {
   }
 }
 
-export const userForgetPasswordSuccess = message => {
-  return {
-    type: FORGET_PASSWORD_SUCCESS,
-    payload: message,
-  }
-}
+
 
 export const userForgetPasswordError = message => {
   return {
